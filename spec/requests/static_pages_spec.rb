68675487a669b657d0cd00329aa4faa2a8e7f30a
lhdv <lh@lhdv.net>
2014-02-23 11:02:46 -0300

    Fix custom stylesheet bug and add custom routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "StaticPages" do
   describe "Home Page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title("| Home")
     end
 
@@ -26,12 +26,12 @@ describe "StaticPages" do
   describe "Help Page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
 
@@ -40,12 +40,12 @@ describe "StaticPages" do
   describe "About Page" do
 
     it "should have the content 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About us')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
 
@@ -54,15 +54,18 @@ describe "StaticPages" do
   describe "Contact Page" do
     
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content("Contact")
     end
  
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
 
   end
 
 end
+
+
+
