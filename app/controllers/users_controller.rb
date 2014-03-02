fd4e841ef29269b0afe8fc86aaf3d122f359923e
lhdv <lh@lhdv.net>
2014-03-02 12:12:43 -0300

    Add logic for signup errors and build its view layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
 
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    # Private method to return params hash using strong parameters new feature
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                   :password_confirmation)
+    end
 
 end
