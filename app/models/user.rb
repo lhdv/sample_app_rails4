0f794125325fa4df375538bb51b76c81b9ecebe4
lhdv <lh@lhdv.net>
2014-03-09 08:51:10 -0300

    Add list of microposts in the root path

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,10 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     # Create the token for the user
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
 
   subject { page }
 
@@ -21,6 +21,22 @@ describe "StaticPages" do
     it_should_behave_like "all static pages"
 
     it { should_not have_title("| Home") }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+	FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+	FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+	sign_in user
+	visit root_path
+      end
+
+      it "should render the user's fedd" do
+	user.feed.each do |item|
+	  expect(page).to have_selector("li##{item.id}", text: item.content)
+	end
+      end
+    end
 
   end
 
